9a912474d9947b697e843587d3185d1d0db47711
Vaibhav Bhuse <bhusevaibhav@gmail.com>
2025-09-15 22:02:24 +0530

    13th Sep 2025, All files added part 1

---
+++ src/ex_02_Java_Basics/Lab12_Variables.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab12_Variables {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab13_Variables.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab13_Variables {
+
+    //data_type variable_Name = variable_value
+
+    byte age_sunit = 45;
+    short age_pramod = 65;
+}
