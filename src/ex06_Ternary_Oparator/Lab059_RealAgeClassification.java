410ebcdaec42a6b409562c9c6cc2ec9c5a5899f1
Vaibhav Bhuse <bhusevaibhav@gmail.com>
2025-10-02 15:55:59 +0530

    14th Sep 2025, Literals and Operator

---
+++ src/ex06_Ternary_Oparator/Lab059_RealAgeClassification.java
@@ -0,0 +1,17 @@
+package ex06_Ternary_Oparator;
+
+public class Lab059_RealAgeClassification {
+    public static void main(String[] args) {
+        String user_input = args[0];
+        System.out.println(user_input);
+        System.out.println(user_input instanceof String);
+
+        //input - String
+        //String - Int
+
+        int age = Integer.parseInt(user_input);
+
+        String result = (age < 18) ? "Minor" : (age <=60) ? "Adult" : "Sr. Citizen";
+        System.out.println(result);
+    }
+}
