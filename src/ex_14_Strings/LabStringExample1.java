7a3dcf4ae8da7c95abce2cd154dadea368fe02f5
Vaibhav Bhuse <bhusevaibhav@gmail.com>
2025-10-19 23:43:00 +0530

    programs 4th Oct

---
+++ src/ex_14_Strings/LabStringExample1.java
@@ -0,0 +1,33 @@
+package ex_14_Strings;
+
+public class LabStringExample1 {
+    public static void main(String[] args) {
+        String s = "Java";
+        char c = s.charAt(2);
+        System.out.println(c);
+
+        int result = "abc".compareTo("abc"); //gives ascii value difference, if same it gives 0
+        System.out.println(result);
+
+        int idx = "Java".indexOf("a");
+        System.out.println(idx);
+
+        int idx2 = "Java".lastIndexOf("a");
+        System.out.println(idx2);
+
+        boolean b = "".isEmpty();
+        System.out.println(b);
+
+        String s11 = String.join("z-","Java","Python");
+        System.out.println(s11);
+
+        String s12 = "Java".replace('a','o');
+        System.out.println(s12);
+
+        boolean b1 = "Java".startsWith("Ja");
+        System.out.println(b1);
+
+        String b2 = "Java".concat("MAVA");
+        System.out.println(b2);
+    }
+}
---
+++ src/ex_14_Strings/LabStringExample2.java
@@ -0,0 +1,9 @@
+package ex_14_Strings;
+
+public class LabStringExample2 {
+    public static void main(String[] args) {
+        CharSequence s = "Vaibhav".subSequence(0,4);
+
+        System.out.println(s);
+    }
+}
