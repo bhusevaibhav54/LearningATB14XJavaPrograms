f47298d5dd17a62fc5063bc1aff2fc96a3f14077
Vaibhav Bhuse <bhusevaibhav@gmail.com>
2025-10-24 23:00:28 +0530

    Ex 11th Oct

---
+++ src/Exercise/Exercise_11thOct/Inverted_Right_Angle.java
@@ -0,0 +1,12 @@
+package Exercise.Exercise_11thOct;
+
+public class Inverted_Right_Angle {
+    public static void main(String[] args) {
+        for (int i = 5; i >0 ; i--) {
+            for (int j = i; j >0; j--) {
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+    }
+}
